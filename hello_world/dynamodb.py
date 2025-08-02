5f87eb3ce80ee89c57963b5f9e75f5604a0d8bd6
jd <jdsanchez2011@gmail.com>
2025-08-02 15:53:45 -0600

    1 row per item model - each row contains list of assigned users

--- hello_world/dynamodb.py
+++ hello_world/dynamodb.py
@@ -3,29 +3,25 @@ import boto3
 
 dynamodb = boto3.resource('dynamodb')
 
-# Function to write receipt items to DynamoDB for each assigned user
 def write_receipt_items_to_dynamodb(receipt_id, items, assigned_users):
-    table_name = 'ReceiptItems'  # Change this to your actual table name
+    table_name = 'ReceiptItems'  # Update to your actual table name
     table = dynamodb.Table(table_name)
-    converted_items = convert_floats(items)
 
     for item in items:
-        for user_id in assigned_users:
-            pk = f"USER#{user_id}"
-            sk = f"RECEIPT#{receipt_id}|ITEM#{converted_items['item_number']}"
-            # sk = f"RECEIPT#{receipt_id}"
-            table.put_item(
-                Item=convert_floats({
-                    'PK': pk,
-                    'SK': sk,
-                    'item_name': converted_items['item'],
-                    'price': converted_items['price'],
-                    'discount': converted_items['discount'],
-                    'receipt_id': receipt_id,
-                    'assigned_users': assigned_users
-                }),
-                ConditionExpression='attribute_not_exists(PK) AND attribute_not_exists(SK)'
-            )
+        pk = f"RECEIPT#{receipt_id}"
+        sk = f"ITEM#{item['item_number']}"
+        table.put_item(
+            Item=convert_floats({
+                'PK': pk,
+                'SK': sk,
+                'item_name': item['item'],
+                'price': item['price'],
+                'discount': item['discount'],
+                'receipt_id': receipt_id,
+                'assigned_users': assigned_users
+            }),
+            ConditionExpression='attribute_not_exists(PK) AND attribute_not_exists(SK)'
+        )
 
 def convert_floats(obj):
     if isinstance(obj, list):
